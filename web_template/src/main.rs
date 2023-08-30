bb7e19fa9ef6e8df1de64aa14fb25dc68cee5cb1
3D ALGO LAB <83936830+3DAlgoLab@users.noreply.github.com>
2023-08-30 15:17:59 +0900

    Update main.rs

--- web_template/src/main.rs
+++ web_template/src/main.rs
@@ -1,8 +1,6 @@
 use actix_cors::Cors;
-use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
 
-use async_trait::async_trait;
-use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
@@ -67,8 +65,59 @@ impl Database {
     fn get_user_by_name(&self, username: &str) -> Option<&User> {
         self.users.values().find(|u: &&User| u.username == username)
     }
+
+    // Db saving
+    fn save_to_file(&self) -> std::io::Result<()> {
+        let json = serde_json::to_string(&self)?;
+        let mut file = fs::File::create("database.json")?;
+        file.write_all(json.as_bytes())?;
+        Ok(())
+    }
+
+    fn load_from_file(&self) -> std::io::Result<Self> {
+        let file_content = fs::read_to_string("database.json")?;
+        let db = serde_json::from_str(&file_content)?;
+        Ok(db)
+    }
 }
 
-fn main() {
-    println!("Hello, world!");
+struct AppState {
+    db: Mutex<Database>,
+}
+
+async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
+    let mut db = app_state.db.lock().unwrap();
+    db.insert(task.into_inner());
+    let _ = db.save_to_file();
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = match Database::new().load_from_file() {
+        Ok(db) => db,
+        Err(_) => Database::new(),
+    };
+
+    let data: web::Data<AppState> = web::Data::new(AppState { db: Mutex::new(db) });
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                Cors::permissive()
+                    .allowed_origin_fn(|origin, _req_head| {
+                        origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
+                    })
+                    .allowed_methods(vec!["GET", "POST", "DELETE"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .supports_credentials()
+                    .max_age(3600),
+            )
+            .app_data(data.clone())
+            .route("/task", web::post().to(create_task))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
